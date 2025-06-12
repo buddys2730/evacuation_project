377867f0bad8b9bd33e5a49c36698d893635cf16
Masashi Takao <masashitakao@MasashinoMacBook-Pro.local>
2025-06-12 10:46:36 +0900

    Chore: format with Prettier and resolve ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,26 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
-    "no-undef": "off"
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'off',
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
