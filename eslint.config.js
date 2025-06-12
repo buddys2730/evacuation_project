0ba9cdc39774dd4b0d1037859ba5d222be589d7e
Masashi Takao <masashitakao@MasashinoMacBook-Pro.local>
2025-06-12 19:03:50 +0900

    Fix: ESLint Flat Configでnode_modulesなど除外

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,21 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-
 export default [
-  js.configs.recommended,
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/*.min.js"
+    ],
+  },
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    plugins: {
-      react
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-vars": "warn"
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
     },
   },
 ];
