05a481fd94fa4c8309784db7e5ffe29b42819fe0
Masashi Takao <masashitakao@MasashinoMacBook-Pro.local>
2025-06-12 19:26:41 +0900

    Fix: Huskyプリコミットから不要なnpm testを削除

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  'src/**/*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+export default {
+  "frontend/**/*.{js,jsx}": ["npx eslint --fix", "npx prettier --write"],
+  "frontend/**/*.md": ["node scripts/validate-md.cjs"],
 };
