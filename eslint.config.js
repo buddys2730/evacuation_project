a20d69405610709e2b7a98209416f094783cacbd
Masashi Takao <masashitakao@MasashinoMacBook-Pro.local>
2025-06-12 15:07:38 +0900

    Fix: CI構成・ESLint・Markdown検証スクリプトを修正

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
 export default [
+  js.configs.recommended,
   {
-    files: ['**/*.{js,cjs}'],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        console: 'readonly'
-      }
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react
     },
     rules: {
-      'no-undef': 'off'
-    }
-  }
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-vars": "warn"
+    },
+  },
 ];
