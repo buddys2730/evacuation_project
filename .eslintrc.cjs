275671a393502faf180addbfe9e28a7624e8011d
Masashi Takao <masashitakao@MasashinoMacBook-Pro.local>
2025-06-12 10:50:08 +0900

    Fix: ESLint config format and ignore node_modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
